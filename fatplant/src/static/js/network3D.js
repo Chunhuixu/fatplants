e8d15565c42f4e62e1446e8f411233bb9318211f
Jo Cali <42591872+jjckv6@users.noreply.github.com>
2020-10-14 13:07:22 -0500

    Implemted json data to 3D network graph

--- fatplant/src/static/js/network3D.js
+++ fatplant/src/static/js/network3D.js
@@ -1,18 +1,17 @@
-    const N = 30;
-    const gData = {
-      nodes: [...Array(N).keys()].map(i => ({ id: i })),
-      links: [...Array(N).keys()]
-        .filter(id => id)
-        .map(id => ({
-          source: id,
-          target: Math.round(Math.random() * (id-1))
-        }))
-    };
-
-
 const Graph = ForceGraph3D()
- (document.getElementById('3d-graph'))
-	.graphData(gData)
-	// .jsonURl('data3D.json')
-	.nodeLabel('id')
-	.nodeAutoColorBy('group');
+      (document.getElementById('3d-graph'))
+        .jsonUrl('data3D.json')
+        .nodeLabel(node => `${node.id} - Value 1: ${node.value1}, Value 2: ${node.value2}`)
+        .linkLabel(link => `EdgeBetweenness: ${link.EdgeBetweenness}, Interaction: ${link.interaction}`)
+        .nodeAutoColorBy('group')
+        // .width(1300)
+        .d3Force('charge').strength(-150)
+        .nodeRelSize(6)
+        .nodeOpacity([.65])
+        .linkWidth(3);
+        
+
+
+    //fitting network to size of container
+    // Graph.cooldownTicks([400]);
+    // Graph.onEngineStop(() => Graph.zoomToFit(400));
